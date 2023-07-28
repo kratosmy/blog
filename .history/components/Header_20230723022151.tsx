5931215d4a72da1ca9d5772d8948569c814916fc
lov3camille <lov3camille@gmail.com>
2023-07-28 22:01:22 +0800

    update name

--- .history/components/Header_20230723022151.tsx
+++ .history/components/Header_20230723022151.tsx
@@ -29,7 +29,7 @@ export default function Header() {
                     }
                   )}
                 >
-                  kratos.
+                  lov3camille.
                 </div>
               </Link>
             </div>
