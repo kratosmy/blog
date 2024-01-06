d5fa378decdcb6c98a89b83cd0cd7b4aec4dbff9
kratosmy <kratosmy@proton.me>
2024-01-06 15:27:53 +0800

    config logrocket

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import LogRocket from '@/components/LogRocket';
 import LenisProvider from '@/components/Providers/LenisProvider';
 import ThemeProvider from '@/components/Providers/ThemeProvider';
 import localFont from 'next/font/local';
+import Script from 'next/script';
 
 const scFont = localFont({
   src: '../public/static/fonts/Noto_Serif_SC/noto-serif-sc-v22-chinese-simplified_latin-regular.woff2',
@@ -27,21 +28,21 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q24QNPTG8X"></script>
-        <script>
-          {`window.dataLayer = window.dataLayer || [];
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+
+        <Script strategy="lazyOnload">
+          {`
+            window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-Q24QNPTG8X');`}
-        </script>
-        <script type="text/javascript">
-          {`(function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "khf0g75qs1");
-          `}
-        </script>
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+         `}
+        </Script>
         <title></title>
       </head>
       <body className="bg-white text-black antialiased dark:bg-black dark:text-white">
