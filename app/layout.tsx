d4eb4b4d5ce0d5a6d456020cc874384ea9bb698e
kratosmy <kratosmy@proton.me>
2024-01-06 12:37:51 +0800

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon.ico" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta name="theme-color" content="#000000" />
+        <script type="text/javascript">
+          {`(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "khf0g75qs1");
+          `}
+        </script>
         <title></title>
       </head>
       <body className="bg-white text-black antialiased dark:bg-black dark:text-white">
