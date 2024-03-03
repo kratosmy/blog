e4554021e7f8863623fe3e3048418a272b3f6708
kratosmy <kratosmy@proton.me>
2024-03-03 12:47:26 +0800

    update yarn to 4.1

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  semi: true,
-  singleQuote: true,
-  printWidth: 100,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'es5',
-  bracketSpacing: true,
-  tailwindConfig: './tailwind.config.js',
+    semi: true,
+    singleQuote: true,
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    tailwindConfig: './tailwind.config.js',
 };
