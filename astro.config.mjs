ac6c5e49860b2f63c6c8709098f8b9899b5c7fa0
kratosmy <kratosmy@proton.me>
2025-01-04 14:34:03 +0800

    add footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,27 @@ import expressiveCode from "astro-expressive-code";
 
 import remarkDirective from "remark-directive";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 import cloudflare from "@astrojs/cloudflare";
+
+import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [yaml()],
     build: {
-      assets: "assets",
+      assetsDir: "assets",
     },
   },
   compressHTML: false,
   experimental: {},
-  serverIslands: true,
-
+  devToolbar: {
+    enabled: true
+  },
   // prefetch: true,
   site: "https://blog.changshaking.com",
   scopedStyleStrategy: "class",
@@ -30,22 +36,20 @@ export default defineConfig({
     format: "directory",
   },
   markdown: {
-    theme: 'everforest-dark',
+    shikiConfig: {
+      theme: 'everforest-dark',
+    },
     syntaxHighlight: 'shiki',
     remarkRehype: {
       footnoteLabel: " ",
     }
   },
 
-  integrations: [
-    UnoCSS(),
-    expressiveCode({
-      themeCssSelector: (theme) => {
-        return "." + theme.type;
-      },
-    }),
-    mdx(),
-  ],
+  integrations: [UnoCSS(), expressiveCode({
+    themeCssSelector: (theme) => {
+      return "." + theme.type;
+    },
+  }), mdx(), sitemap(), partytown(), icon()],
   output: "server",
   adapter: cloudflare(),
 });
